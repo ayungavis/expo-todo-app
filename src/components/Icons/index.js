14145f06866ec6ba1fec9b62717109bcca13edba
Wahyu Kurniawan <ayungavis@gmail.com>
2023-06-25 02:19:45 +0700

    ⚡️ feat: add assets and icons

---
+++ src/components/Icons/index.js
@@ -0,0 +1,7 @@
+import MenuIcon from "./menu.svg";
+import EditIcon from "./edit.svg";
+import FilterIcon from "./filter.svg";
+import PlusIcon from "./plus.svg";
+import BackIcon from "./back.svg";
+
+export { MenuIcon, EditIcon, FilterIcon, PlusIcon, BackIcon };
